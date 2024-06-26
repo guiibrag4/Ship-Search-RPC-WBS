10daa9c4d928b32b2575da811642ec1a41628d1b
Kelvin Lima <kelvinl65unieuro@gmail.com>
2024-06-26 19:22:04 -0300

    Adicionado documentos e apresentação

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -21,7 +21,6 @@ def lidar_com_imagem(data):
     print('Imagem recebida pelo WebSocket')
     image_base64 = data['image']
     
-    # Enviar imagem para o servidor RPC
     resposta = requests.post('http://127.0.0.1:5000/detectar', files={'file': base64.b64decode(image_base64)})
     if resposta.status_code == 200:
         resultado = resposta.json()
